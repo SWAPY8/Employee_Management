943c2c9b1529800c3e03547946effb5076f18060
Swapnil <swapnilofficial08@gmail.com>
2026-09-15 14:19:44 +0530

    Frontend added

--- Controller/Employee_Controller.py
+++ Controller/Employee_Controller.py
@@ -3,44 +3,54 @@ from Model.Update_Employee_Struct import update_struct
 from Database.Employee_DB import employee_collection
 
 def get_employee():
-    all_data = list(employee_collection.find({}, {"_id" : 0}))  
+    all_data = list(employee_collection.find({}, {"_id": 0}))  
     return all_data 
 
-def create_employee(employee:struct):
+def create_employee(employee: struct):
     s_id = employee.e_id
     s_name = employee.e_name
     s_dept = employee.e_dept
     s_email = employee.e_email
     
     sdict = {
-        "e_id" : s_id ,
-        "e_name" : s_name,
-        "e_dept" : s_dept,
-        "e_email" : s_email
+        "e_id": s_id,
+        "e_name": s_name,
+        "e_dept": s_dept,
+        "e_email": s_email
     }
     
     employee_collection.insert_one(sdict)
 
-    return   {"message":"employee Created!!!"}
+    return {"message": "employee Created!!!"}
 
-def update_employee(e_id : int, employee_update:struct):
+def update_employee(e_id: int, employee_update: update_struct):
     updated_data = employee_update.model_dump(
-        exclude_none = True 
+        exclude_none=True 
     )    
     
-    e_data = employee_collection.find_one({"e_id" : e_id })
+    try:
+        query_id = int(e_id)
+    except (ValueError, TypeError):
+        query_id = e_id
+
+    e_data = employee_collection.find_one({"e_id": {"$in": [query_id, str(e_id)]}})
     if e_data is None:
-        return {"No such employee Exist..."}
+        return {"message": "No such employee Exist..."}
     else:
         employee_collection.update_one(
-            {"e_id":e_id},
-            {"$set":updated_data}
+            {"e_id": e_data["e_id"]},
+            {"$set": updated_data}
         )
-    return {"message":"Employee details updated successfully!!!" }
+    return {"message": "Employee details updated successfully!!!"}
 
-def delete_employee(e_id:int):
+def delete_employee(e_id: int):
+    try:
+        query_id = int(e_id)
+    except (ValueError, TypeError):
+        query_id = e_id
+
     result = employee_collection.delete_one(
-        {"e_id": e_id}
+        {"e_id": {"$in": [query_id, str(e_id)]}}
     )
 
     if result.deleted_count == 0:
@@ -51,7 +61,3 @@ def delete_employee(e_id:int):
     return {
         "message": "Employee deleted successfully !!!"
     }
-        
-        
-    
-
--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from Routes.Employee_Routes import router
 
-app = FastAPI()
+app = FastAPI(title="Employee Management System API")
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
 
 @app.get("/")
 def welcome():
-    return {"Welcome! Welcome! Welcome! "}
+    return {"message": "Welcome to Employee Management API"}
